0300f07007addf9c45f3b64902390bf9dd12ada2
anthonylan <anthonylannn@gmail.com>
2023-01-03 23:19:46 +0100

    swap netlify/deno for vercelo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
-import deno from '@astrojs/deno';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
   output: "server",
-  adapter: deno()
+  adapter: vercel()
 });
